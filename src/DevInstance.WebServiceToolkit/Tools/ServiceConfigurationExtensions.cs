4fa2daf31241ef837923675d0a6ba1b2bd570be5
yfranz <yfranz@gmail.com>
2025-01-02 10:50:52 -0800

    Added WebServiceAttribute

---
+++ src/DevInstance.WebServiceToolkit/Tools/ServiceConfigurationExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace DevInstance.WebServiceToolkit.Tools;
+
+public static class ServiceConfigurationExtensions
+{
+    public static void AddServerWebServices(this IServiceCollection services)
+    {
+        foreach (var type in GetTypesWithHelpAttribute(Assembly.GetCallingAssembly()))
+        {
+            services.AddScoped(type);
+        }
+    }
+
+    static IEnumerable<Type> GetTypesWithHelpAttribute(Assembly assembly)
+    {
+        foreach (Type type in assembly.GetTypes())
+        {
+            if (type.GetCustomAttributes(typeof(WebServiceAttribute), true).Length > 0)
+            {
+                yield return type;
+            }
+        }
+    }
+}
