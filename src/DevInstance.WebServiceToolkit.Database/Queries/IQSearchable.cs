34a45360143f91ae95bc364f8d7fde1db05729b9
yfranz <yfranz@gmail.com>
2025-03-14 20:52:58 -0700

    Improved IQSearchable

--- src/DevInstance.WebServiceToolkit.Database/Queries/IQSearchable.cs
+++ src/DevInstance.WebServiceToolkit.Database/Queries/IQSearchable.cs
@@ -5,18 +5,22 @@
 /// Most of the queries working with entitles with public id should implement this interface.
 /// </summary>
 /// <typeparam name="T"></typeparam>
-public interface IQSearchable<T>
+public interface IQSearchable<T, K>
 {
     /// <summary>
     /// Lookup by public id
     /// </summary>
     /// <param name="id">public id</param>
     /// <returns>returns the reference itself</returns>
-    T ByPublicId(string id);
+    T ByPublicId(K id);
     /// <summary>
     /// Search function
     /// </summary>
     /// <param name="search">search keyword</param>
     /// <returns>returns the reference itself</returns>
     T Search(string search);
+}
+
+public interface IQSearchable<T> : IQSearchable<T, string>
+{
 }
